4ce8fd897d34a515e0bb52baa90e2603d340a230
akankshafunde24 <akankshafunde@gmail.com>
2026-08-27 22:58:52 +0530

    Use approved Jankar PNG logo asset

--- logo.js
+++ logo.js
@@ -1,2 +1,2 @@
-// Shared Jankar India logo asset. Keep the text lock-up in the existing brand component.
-const LOGO_DATA="assets/jankar-logo.svg";
+// Shared Jankar India logo asset.
+const LOGO_DATA="assets/jankar-logo.png";
